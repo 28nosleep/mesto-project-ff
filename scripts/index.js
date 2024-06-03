9fb0058e7c054ffab78eafa198550498a89f54fb
Nikita Astrov <28nosleep@gmail.com>
2024-06-03 22:06:26 +1000

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,33 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCard(cardData, deleteCallback) {
+  const cardTemplate = document.getElementById("card-template").content;
+  const cardElement = cardTemplate.cloneNode(true).querySelector(".card");
+
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteCallback(cardElement);
+  });
+
+  return cardElement;
+}
+
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+const placesList = document.querySelector(".places__list");
+
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData, deleteCard);
+  placesList.appendChild(cardElement);
+});
